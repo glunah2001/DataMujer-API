9579e1b6d4601af13ae5349f685adafc789aa065
AHKolodin <ahkolodin@gmail.com>
2025-09-12 15:58:38 -0600

    Se agregó el cambio de contraseña Ahora los usuarios pueden restablecer su contraseña en dos pasos. Primero envían su correo para obtener un token de reseteo y luego en la aplicación colocan su nueva contraseña junto al token.

--- src/main/java/com/UNED/APIDataMujer/controller/AuthController.java
+++ src/main/java/com/UNED/APIDataMujer/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.UNED.APIDataMujer.controller;
 
+import com.UNED.APIDataMujer.dto.authentication.ResetPasswordDTO;
 import com.UNED.APIDataMujer.dto.authentication.UserLoginDTO;
 import com.UNED.APIDataMujer.dto.token.TokenResponse;
 import com.UNED.APIDataMujer.service.authentication.AuthServiceImpl;
+import com.UNED.APIDataMujer.service.authentication.PasswordResetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthServiceImpl authService;
+    private final PasswordResetService passwordResetService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody final UserLoginDTO loginDTO){
@@ -27,4 +30,18 @@ public class AuthController {
         final TokenResponse token = authService.refresh(authHeader);
         return ResponseEntity.ok(token);
     }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestBody final String email){
+        passwordResetService.forgotPassword(email);
+        return ResponseEntity.ok("Si el correo se encuentra registrado, se ha enviado un correo de " +
+                "recuperación de contraseña.");
+    }
+
+    @PostMapping("reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody final ResetPasswordDTO resetPasswordDTO){
+        passwordResetService.resetPassword(resetPasswordDTO);
+        return ResponseEntity.ok("Contraseña restablecida con éxito");
+    }
+
 }
---
+++ src/main/java/com/UNED/APIDataMujer/service/authentication/PasswordResetService.java
@@ -0,0 +1,72 @@
+package com.UNED.APIDataMujer.service.authentication;
+
+import com.UNED.APIDataMujer.dto.authentication.ResetPasswordDTO;
+import com.UNED.APIDataMujer.entity.Token;
+import com.UNED.APIDataMujer.enums.TokenType;
+import com.UNED.APIDataMujer.mapper.TokenMapper;
+import com.UNED.APIDataMujer.mapper.UserMapper;
+import com.UNED.APIDataMujer.repository.TokenRepository;
+import com.UNED.APIDataMujer.repository.UserRepository;
+import com.UNED.APIDataMujer.service.emailing.EmailSendingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetService {
+
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
+    private final TokenMapper tokenMapper;
+
+    private final EmailSendingService emailSendingService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void forgotPassword(final String email){
+        final var user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("El email: "+email+" no se encuentra registrado a " +
+                        "ningún usuario actualmente."));
+
+        long expiration = Instant.now().plus(15, ChronoUnit.MINUTES).toEpochMilli();
+        final String resetToken = UUID.randomUUID().toString() +"_"+ expiration;
+        final Token token = tokenMapper.toEntity(resetToken, user, TokenType.PASSWORD_RESET);
+        tokenRepository.save(token);
+
+        String message = String.format("Este es su token de restablecimiento de contraseña: %s.\n" +
+                "Ingrese dicho token en el espacio indicado en la aplicación junto a su nueva contraseña " +
+                "para hacer efectivo el cambio.",resetToken);
+
+        emailSendingService.sendEmail(email, "Restablecimiento de Contraseña", message);
+    }
+
+    public void resetPassword(final ResetPasswordDTO dto){
+        final var token = tokenRepository.findByToken(dto.token())
+                .orElseThrow(() ->
+                        new IllegalArgumentException("El token ingresado no es válido"));
+
+        if(token.isExpired() || token.isRevoked()){
+            throw new IllegalArgumentException("Este token de restablecimiento de contraseña ha caducado.");
+        }
+
+        String[] parts = token.getToken().split("_");
+        long expiration = Long.parseLong(parts[1]);
+
+        if(Instant.now().toEpochMilli() > expiration){
+            throw new IllegalArgumentException("Este token de restablecimiento de contraseña ha caducado.");
+        }
+
+        var user = token.getUser();
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        userRepository.save(user);
+    }
+
+}
