b2a3e1978b363f301420f9d86f08dd98f58c8ebf
AHKolodin <ahkolodin@gmail.com>
2025-09-24 18:31:51 -0600

    Pequeño cambio en el registro de usuarios aprovechando CascadeType.ALL de User sobre Person

--- src/main/java/com/UNED/APIDataMujer/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.UNED.APIDataMujer.repository;
-
-import com.UNED.APIDataMujer.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
